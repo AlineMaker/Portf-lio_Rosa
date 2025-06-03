67868738f4e011301f84b1fb5758ba52dc61bbe6
Aline Alves de Souza <alineconfidence@gmail.com>
2025-06-03 20:44:43 -0300

    header add

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+ content:["./*.html"]
+  theme: {
+    extend: {
+      fontFamily: {
+        gilroy: ['gilroy-regular', 'sans-serif'],
+        gilroyBold: ['gilroy-bold', 'sans-serif'],
+      },
+    },
+  },
+};
